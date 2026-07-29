5d265d07937b3848cda909e97b6e39314561c7b6
Guy Asong <guyasong1@gmail.com>
2026-07-29 19:00:58 +0100

    Added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import WhatsAppButton from "@/components/site/WhatsAppButton";
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
+
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://starlabsgroup.com"),
@@ -103,6 +105,7 @@ export default function RootLayout({
       <body className="min-h-screen flex flex-col antialiased">
         {children}
         <WhatsAppButton />
+        <Analytics />
       </body>
     </html>
   );
